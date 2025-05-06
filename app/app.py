0303b1790c0332c2636ac80feeae920639ce92cd
Marvin Blommestijn <M.J.C.Blommestijn-1@student.tudelft.nl>
2025-05-06 15:39:25 +0200

    added model-service integration and frontend

--- app/app.py
+++ app/app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+from lib_version.version_util import VersionUtil
+import requests
+import os
 
 app = Flask(__name__)
+MODEL_URL = os.environ.get('MODEL_SERVICE_URL', 'http://model-service:5001')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/version/appversion')
-def get_lib_version():
-    return jsonify({'message': 'Here should be App Version!'})
+
+@app.route('/sentiment', methods=['POST'])
+def sentiment():
+    text = request.form['text']
+    resp = requests.get(f"{MODEL_URL}/predict", params={'text': text})
+    data = resp.json()
+    return jsonify(sentiment=data['sentiment'])
+
+@app.route('/version')
+def version():
+    return jsonify(app_version=VersionUtil.get_version())
 
 @app.route('/version/modelversion')
-def get_model_version():
-    return jsonify({'message': 'Here should be Model Version!'})
+def modelversion():
+    # proxy to model-service
+    resp = requests.get(f"{MODEL_URL}/version")
+    data = resp.json()
+    return jsonify(model_service_version=data.get('service_version'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
